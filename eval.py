de9d5757bc4af6e463a1fb1193d152c75ed8b1e2
Animesh <35520996+Animesh98@users.noreply.github.com>
2022-03-10 11:56:48 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -31,7 +31,7 @@ def get_opts():
                         help='test or test_train')
     parser.add_argument('--img_wh', nargs="+", type=int, default=[800, 800],
                         help='resolution (img_w, img_h) of the image')
-    parser.add_argument('--spheric_poses', default=False, action="store_true",
+    parser.add_argument('--spheric_poses', default=True, action="store_true",
                         help='whether images are taken in spheric poses (for llff)')
 
     parser.add_argument('--N_samples', type=int, default=64,
